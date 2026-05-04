4fc3f4920e3ce0a5ab5e3566ef59c300650e50b0
abdulalilm1010 <193335434+abdulalim1010@users.noreply.github.com>
2026-05-04 22:06:08 +0600

    add to the syntax

---
+++ class.ts
@@ -0,0 +1,17 @@
+class Animal{
+  name: string;
+  specis: string;
+  sound: string
+  constructor(name:string,specis:string,sound:string
+    
+  )
+
+  {
+    this.name = name,
+      this.sound = sound,
+      this.specis=specis
+    }
+}
+const dog = new Animal("mithu", "Ghew", "dog")
+console.log(dog.name)
+
